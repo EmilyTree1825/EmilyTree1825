c9ce5786acd522e35fcd5cf96292344df43212c9
Dr Bryon Christopher <cissp@bryongloden.com>
2015-09-23 16:05:57 -0400

    1090127045
    This version is 1090127045 epoch and is now certified and is now
    accredited for space delta and space-x by CISSP 443121

--- AmyTree1825/main.cpp
+++ AmyTree1825/main.cpp
@@ -22,6 +22,15 @@ int main ()
     char word[3];
     strncpy(word, "now", 3);
     // PERSONA NON-GRATA
-    printf ("This version is %.f epoch and is %s certified and is %s accredited for ""space delta"" and ""space-x"" by CISSP.", amytree1825, word, word);
+    printf ("This version is %.f epoch and is %s certified and is %s accredited for ""space delta"" and ""space-x"" by CISSP 443121\n", amytree1825, word, word);
+    return(0);
 }
 
+// [1] #DoD
+// [2] http://stackoverflow.com/questions/2425167/use-of-exit-function
+// [3] http://stackoverflow.com/questions/20026517/program-ends-with-exit-code-0-immediately-but-it-shouldnt-get-there-without-i
+// [4] https://discussions.apple.com/thread/5360967
+// [5] https://forums.developer.apple.com/thread/9949
+// [6] #REDACTED
+// [7] http://www.raywenderlich.com/114173/learn-to-code-ios-apps-with-swift-tutorial-2-your-first-project
+// [8] #Safeware
